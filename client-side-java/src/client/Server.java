1a98dfe753641b7994e6c89fa9fdfe0c7ba94d20
davidshushan <93195038+davidshushan@users.noreply.github.com>
2022-07-08 16:18:38 +0300

    Add files via upload

---
+++ client-side-java/src/client/Server.java
@@ -0,0 +1,65 @@
+package client;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+
+    private static ServerSocket serverSocket;
+
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void initializeServer() {
+
+        try {
+            while (!serverSocket.isClosed()) {
+
+                Socket socket = serverSocket.accept();
+//                System.out.println(" a new connection establish with " + socket.getLocalAddress());
+                System.out.println(" a new connection establish with " + socket.getInetAddress());
+
+                client.SocketHandler socketHandler;
+                socketHandler = new SocketHandler(socket);
+
+                Thread thread = new Thread(socketHandler);
+                thread.start();
+
+
+            }
+        } catch (IOException e) {
+
+        }
+
+    }
+
+    public static void closeSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5004);
+            Server myServer = new Server(serverSocket);
+            myServer.initializeServer();
+            System.out.println("server is running");
+        } catch (IOException e) {
+
+            System.out.println("system failure: failed to initialize the server");
+            System.out.println(e.getCause());
+            e.printStackTrace();
+            closeSocket();
+
+        }
+    }
+}
